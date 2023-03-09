81b75a90b70d8ff54209d237eeb7a7b70d6b7154
zumouse <mouse_zy@qq.com>
2023-03-09 16:17:34 +0800

    feat:超声波驱动实车测试完成

---
+++ ros2_ultra_sonic_radar_driver/src/ultra_sonic_radar_driver_main.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file ultra_sonic_radar_driver_main.cpp
+ * @author zumoude (zymouse@pixmoving.net)
+ * @brief ultra sonic radar driver 
+ * @version 1.0
+ * @date 2023-03-08
+ * 
+ * @copyright Copyright (c) 2022, Pixmoving
+ * Rebuild The City With Autonomous Mobility
+ * https://www.pixmoving.com
+ * 
+ */
+#include "ros2_ultra_sonic_radar_driver/ultra_sonic_radar_driver.hpp"
+
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node=std::make_shared<ultra_sonic_radar_driver::UltraSonicRadarDriver>("ultra_sonic_radar_driver_node");
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
